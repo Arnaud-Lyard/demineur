7d8b0b2b6433d4ae665d9521d283ff75e98e9f21
Arnaud-Lyard <arnaud.lyard@gmail.com>
2023-02-28 12:26:23 +0100

    ✨ Create empty board

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Board, cheating, createEmptyBoard } from "./Board";
 function App() {
+  const [board, setBoard] = useState<Board>([]);
+  useEffect(() => {
+    setBoard(createEmptyBoard(5));
+  }, []);
   return (
     <div className="App">
       <div>
-        <button>Cheating</button>
-        <button>Reload</button>
+        <button onClick={() => setBoard(cheating(board))}>Cheating</button>
+        <button onClick={() => setBoard(createEmptyBoard(5))}>Reload</button>
         <table>
           <tbody>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
+            {board.map((row, rowIndex) => {
+              return (
+                <tr key={rowIndex}>
+                  {row.map((cell, cellIndex) => {
+                    return (
+                      <td key={cellIndex}>{cell.revealed ? cell.val : ""}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
